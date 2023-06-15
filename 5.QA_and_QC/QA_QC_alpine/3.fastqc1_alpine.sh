d68850c0b71a71fb928c269fbdf10b18b55c06a2
Emily <104112036+emilynyeo@users.noreply.github.com>
2023-06-15 17:50:20 -0600

    removed some unnecessary stuff 3.fastqc1_alpine.sh

--- 5.QA_and_QC/QA_QC_alpine/3.fastqc1_alpine.sh
+++ 5.QA_and_QC/QA_QC_alpine/3.fastqc1_alpine.sh
@@ -16,20 +16,17 @@ conda activate /projects/$USER/software/anaconda/envs/metaair_env
 export PATH=$PATH:/curc/sw/install/bio/bedtools/2.29.1/bin/
 export PATH=$PATH:/curc/sw/install/bio/bbtools/bbmap/
 export PATH=$PATH:/scratch/alpine/emye7956/MetaAir/FastQC/
-export PATH=$PATH:/scratch/alpine/emye7956/MetaAir/Trimmomatic-0.39/
-export PHYLOFLASH_DBHOME=/scratch/alpine/emye7956/MetaAir/phyloFlash-pf3.4.2/138.1
+
 cd /scratch/alpine/emye7956/mothersmilk/mg_data_labmac/fastq
 
 export outer_dir=/scratch/alpine/emye7956/mothersmilk/mg_data_labmac/fastq/
-export trimmomatic_loc=/scratch/alpine/emye7956/MetaAir/Trimmomatic-0.39/trimmomatic-0.39.jar
-export phyloflash_loc=/scratch/alpine/emye7956/MetaAir/phyloFlash-pf3.4.2/phyloFlash.pl
+
 for outer_folder in "$outer_dir"*; do
     for infile in $(ls "$outer_folder/"*_1.fq.gz); do
         inner_dirname=$(basename $outer_folder);
         new_dirname=/scratch/alpine/emye7956/mothersmilk/mg_data_labmac/fastq/$inner_dirname
 
         mkdir -pv $new_dirname
-        mkdir -pv $new_dirname/trimmed_output
 
         base=$(basename ${infile} _1.fq.gz)
         outer_path=$(dirname ${infile})
